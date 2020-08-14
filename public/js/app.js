8bc57812bf2cc725ca9e23c264407353b0ab6266
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2020-08-14 18:34:19 +0530

    Removed an unnecessary log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("This is the client Side javascript file")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const forecast = document.querySelector('#forecast')
